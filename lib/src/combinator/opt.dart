ed360df64094197831125d40db124b2a5c85d8a8
mezoni <andrew.mezoni@gmail.com>
2022-04-13 19:16:03 +0500

    0.16.1

--- lib/src/combinator/opt.dart
+++ lib/src/combinator/opt.dart
@@ -1,19 +1,13 @@
 part of '../../combinator.dart';
 
-class Opt<I, O> extends ParserBuilder<I, O?> {
+class Opt<I, O> extends Redirect<I, O?> {
   final ParserBuilder<I, O> parser;
 
   const Opt(this.parser);
 
   @override
-  BuidlResult build(
-      Context context, CodeGen code, ParserResult result, bool silent) {
-    return Silent<I, O?>(Alt2(parser, Value(null)))
-        .build(context, code, result, silent);
-  }
-
-  @override
-  bool isAlwaysSuccess() {
-    return true;
+  ParserBuilder<I, O?> getRedirectParser() {
+    final parser = Silent<I, O?>(Alt2(this.parser, Value(null)));
+    return parser;
   }
 }
