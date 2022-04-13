f532f0b3bc51ae6f1ba9d5944ed558cca1e6a735
mezoni <andrew.mezoni@gmail.com>
2022-04-13 19:13:08 +0500

    0.16.1

--- lib/src/bytes/take_until1.dart
+++ lib/src/bytes/take_until1.dart
@@ -7,18 +7,16 @@ part of '../../bytes.dart';
 /// ```dart
 /// TakeUntil1('{{')
 /// ```
-class TakeUntil1 extends StringParserBuilder<String> {
+class TakeUntil1 extends Redirect<String, String> {
   final String tag;
 
   const TakeUntil1(this.tag);
 
   @override
-  BuidlResult build(
-      Context context, CodeGen code, ParserResult result, bool silent) {
+  ParserBuilder<String, String> getRedirectParser() {
     final tag = helper.escapeString(this.tag);
     final message = 'Expected at least one character before $tag';
     final verify = ExprAction<bool>(['index'], '{{index}} > state.pos');
-    return Recognize(MoveTo(Verify(message, FindTag(this.tag), verify)))
-        .build(context, code, result, silent);
+    return Recognize(MoveTo(Verify(message, FindTag(this.tag), verify)));
   }
 }
